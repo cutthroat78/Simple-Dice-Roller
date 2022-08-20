c240649977ce8fec3c32fe6205ce45dcc33f5418
CutThroat <55604778+cutthroat78@users.noreply.github.com>
2022-08-20 21:03:03 +0100

    Add files via upload

---
+++ Dice Roller 2.0.py
@@ -0,0 +1,4 @@
+import random
+number = input("What type of dice would you like to roll?")
+result = random.randint(1,number)
+print(result)
---
+++ Dice Roller.py
@@ -0,0 +1,5 @@
+import random
+print("What type of dice would you like to roll?")
+number = input()
+result = random.randint(1,number)
+print(result)
